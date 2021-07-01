2eabf33346bc8c96e118954c934d19747b7c439b
Jose Tom Vettom <84711737+jtv0013@users.noreply.github.com>
2021-07-01 12:32:46 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,33 @@
+
+
+
+void swap_max(int arr[], int l, int n )
+{
+
+int max,t=n,i,j=0;
+max=arr[n];
+for(i=n;i<l;i++)
+{
+if(arr[i]>max)
+{
+max=arr[i];
+t=i;
+}
+j=arr[t];
+arr[t]=arr[n];
+arr[n]=j;
+}
+
+
+}
+
+void ssort(int arr[], int l);
+{
+for(int i=0;i<l;i++)
+{
+swap_max(arr, l, i);
+
+}
+
+
+}
